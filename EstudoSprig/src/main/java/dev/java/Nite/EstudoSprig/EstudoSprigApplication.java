f18983782106cb5fe485e9e3e335d95426d9a353
Diego-Eduardo-da-Silva-Santos <diegodidi2005@gmail.com>
2025-07-03 19:03:46 -0300

    Commit Inicial

---
+++ EstudoSprig/src/main/java/dev/java/Nite/EstudoSprig/EstudoSprigApplication.java
@@ -0,0 +1,13 @@
+package dev.java.Nite.EstudoSprig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoSprigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoSprigApplication.class, args);
+	}
+
+}
---
+++ EstudoSprig/src/test/java/dev/java/Nite/EstudoSprig/EstudoSprigApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java.Nite.EstudoSprig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoSprigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
